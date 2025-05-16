4e9704c0937ca5b7751843ce75ed22edf430b151
MarioIvanov99 <marioivanov998@gmail.com>
2025-05-16 10:26:05 +0300

    Created User entity

--- src/main/java/com/mario/userservice/data/entity/User.java
+++ src/main/java/com/mario/userservice/data/entity/User.java
@@ -1,4 +1,36 @@
 package com.mario.userservice.data.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 45)
+    @NotBlank
+    @Size(min = 5, max = 45)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    @NotBlank
+    @Email
+    @Size(min = 5, max = 100)
+    private String email;
 }
