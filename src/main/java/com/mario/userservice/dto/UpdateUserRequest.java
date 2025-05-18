f5ddec19a0eb7044ba7d96bbfbff5d5416bbb5a1
MarioIvanov99 <marioivanov998@gmail.com>
2025-05-18 09:02:25 +0300

    Create UpdateUserRequest DTO

--- src/main/java/com/mario/userservice/dto/UpdateUserRequest.java
+++ src/main/java/com/mario/userservice/dto/UpdateUserRequest.java
@@ -1,4 +1,21 @@
 package com.mario.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateUserRequest {
+    @NotBlank
+    @Size(min = 5, max = 45)
+    private String username;
+
+    @Email
+    @Size(min = 5, max = 100)
+    private String email;
 }
