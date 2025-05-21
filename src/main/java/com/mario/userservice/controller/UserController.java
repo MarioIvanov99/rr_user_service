8f05b07b845c54c8f7a452cdec77a31e74c2941b
MarioIvanov99 <marioivanov998@gmail.com>
2025-05-21 10:13:58 +0300

    Implemented the controller

--- src/main/java/com/mario/userservice/controller/UserController.java
+++ src/main/java/com/mario/userservice/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.mario.userservice.controller;
 
+import com.mario.userservice.dto.UpdateUserRequest;
+import com.mario.userservice.dto.UserAdminResponse;
+import com.mario.userservice.dto.UserResponse;
+import com.mario.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Validated
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+    @GetMapping("/admin/{id}")
+    public ResponseEntity<UserAdminResponse> findUserAdminById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findUserAdminById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserAdminResponse>> findAllUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<UserResponse> getCurrentUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+        return ResponseEntity.ok(userService.updateUser(id, updateUserRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
